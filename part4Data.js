4a351a1f662b8130fcaa4f6570c35bb877afe4cc
WonderOfU <lebilash@email.com>
2026-05-16 21:40:08 +0300

    Database addition: aqua neclace, bad company, love deluxe and red hot chili peppers

--- part4Data.js
+++ part4Data.js
@@ -41,5 +41,48 @@ const part4Data = {
         stats: "ts_stats.png",
         link: "https://www.youtube.com/watch?v=_Sz2HOAb54w",
         power: "Surface's primary ability is to copy the appearance of another person. If the mannequin is touched by someone, it will perfectly copy that person's appearance, voice, and mannerisms, down to the fingerprints, as noted by Josuke. However, a screw on Surface's forehead enables people to tell them apart. As a Stand bound to a mannequin, no damage done to Surface will be reflected in Hazamada."
-    }
+    },
+    "Aqua Necklace": {
+        image: "aq_stand.png",
+        stats: "aq_stats.png",
+        link: "https://www.youtube.com/watch?v=0tjmAAdTrlM",
+        power: "Aqua Necklace's primary ability is a completely liquid and malleable form, allowing it to sneak, infiltrate, and inflict lethal damage from the inside."
+    },
+    "Bad Company": {
+        image: "bc_stand.png",
+        stats: "bc_stats.png",
+        link: "https://www.youtube.com/watch?v=JXQJpyQBShU",
+        power: "Bad Company is a battalion of miniature soldiers and military vehicles and is equipped as such. Each of Bad Company's soldiers is armed with an M16 assault rifles, a parachute, and combat knives. The Green Berets have the same equipment as the soldiers, but with the addition of a rappelling rope which allows them to climb objects or descend onto targets from above. The helicopters are armed with machine guns and missiles, and the tanks have scaled-down shells. Bad Company is also equipped with land mines"
+    },
+    "Love Deluxe": {
+        image: "ld_stand.png",
+        stats: "ld_stats.png",
+        link: "https://www.youtube.com/watch?v=CtthQ_JotFU",
+        power: "Love Deluxe is bound to Yukako's hair, allowing it to grow to various lengths and move with considerable strength and dexterity. Love Deluxe is precise enough that the hair can tie something as small as a tongue, but is also powerful enough to completely bind and immobilize someone. When angered, Yukako can unleash Love Deluxe and make her hair grow so long, it can cover and invade an entire house"
+    },
+    "Red Hot Chili Peppers": {
+        image: "rhcp_stand.png",
+        stats: "rhcp_stats.png",
+        link: "https://www.youtube.com/watch?v=YlUKcNNmywk",
+        power: "Red Hot Chili Pepper is a long range Stand based on electricity whose dangerousness increases dramatically when it is near electricity. Not only does its range and ability to pass through electrical systems make Otoishi impossible to find, when powered-up, it can physically overwhelm and outpace Crazy Diamond.[4][5] On the other hand, it is very dependent on its power source to act."
+    },
+    "Echoes ACT 2": {
+        image: "cd_stand.png",
+        stats: "cd_stats.png",
+        link: "https://www.youtube.com/watch?v=cWGE9Gi0bB0",
+        power: "Crazy Diamond's unique ability is to restore objects or organisms through touch. It can repair the damage, heal injuries, revert chemical changes and complex structures to their raw components (e.g. a plate of spaghetti back into its raw ingredients). It may also fuse objects, for example, a person with a rock (as with villain Anjuro Katagiri). When restoring a divided object, the user is able to control which part acts as the starting points for the rest to gather towards."
+    },
+    "Crazy Diamond": {
+        image: "cd_stand.png",
+        stats: "cd_stats.png",
+        link: "https://www.youtube.com/watch?v=cWGE9Gi0bB0",
+        power: "Crazy Diamond's unique ability is to restore objects or organisms through touch. It can repair the damage, heal injuries, revert chemical changes and complex structures to their raw components (e.g. a plate of spaghetti back into its raw ingredients). It may also fuse objects, for example, a person with a rock (as with villain Anjuro Katagiri). When restoring a divided object, the user is able to control which part acts as the starting points for the rest to gather towards."
+    },
+    "Crazy Diamond": {
+        image: "cd_stand.png",
+        stats: "cd_stats.png",
+        link: "https://www.youtube.com/watch?v=cWGE9Gi0bB0",
+        power: "Crazy Diamond's unique ability is to restore objects or organisms through touch. It can repair the damage, heal injuries, revert chemical changes and complex structures to their raw components (e.g. a plate of spaghetti back into its raw ingredients). It may also fuse objects, for example, a person with a rock (as with villain Anjuro Katagiri). When restoring a divided object, the user is able to control which part acts as the starting points for the rest to gather towards."
+    },
+
 };
