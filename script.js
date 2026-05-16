002f9d0a936528b802e130e21abe5c0e2a1ba070
WonderOfU <lebilash@email.com>
2026-05-16 17:19:45 +0300

    restructure data layout into dynamic path routing matching local image folders

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-const standData = {
-    "Crazy Diamond": {
-        image: "cd_stand.png",
-        stats: "cd_stats.png",
-        link: "https://www.youtube.com/watch?v=cWGE9Gi0bB0",
-        power: "Crazy Diamond's unique ability is to restore objects or organisms through touch. It can repair the damage, heal injuries, revert chemical changes and complex structures to their raw components (e.g. a plate of spaghetti back into its raw ingredients). It may also fuse objects, for example, a person with a rock (as with villain Anjuro Katagiri). When restoring a divided object, the user is able to control which part acts as the starting points for the rest to gather towards. Feats of this nature include trapping an enemy by restoring pieces of a broken crate around him; exposing a Stand formerly bound to an object; and tracking by restoring a severed hand, forcing it to seek out and reattach itself to the body from which it was cut off."
+const standDatabase = {
+    part4: {
+        "Crazy Diamond": {
+            image: "cd_stand.png",
+            stats: "cd_stats.png",
+            link: "https://www.youtube.com/watch?v=cWGE9Gi0bB0",
+            power: "Crazy Diamond's unique ability is to restore objects or organisms through touch. It can repair the damage, heal injuries, revert chemical changes and complex structures to their raw components (e.g. a plate of spaghetti back into its raw ingredients). It may also fuse objects, for example, a person with a rock (as with villain Anjuro Katagiri). When restoring a divided object, the user is able to control which part acts as the starting points for the rest to gather towards. Feats of this nature include trapping an enemy by restoring pieces of a broken crate around him; exposing a Stand formerly bound to an object; and tracking by restoring a severed hand, forcing it to seek out and reattach itself to the body from which it was cut off."
+        },
+        "The Hand": {
+            image: "hand_stand.png",
+            stats: "hand_stats.png",
+            link: "https://www.youtube.com/watch?v=FFqb1I-hiHE",
+            power: "Anything The Hand's right palm touches when it does a swiping motion with its arm is erased from this universe, be it physical matters or space itself. Okuyasu states that not even he knows where the voided objects go. Objects that are subjected to the ability appear as if an extremely sharp blade had sliced clean through them. Whenever The Hand erases something, the resulting voided space is unable to sustain itself, and reality will immediately be filled and 'stitched up' by adjacent space and objects, effectively removing any traces of the removed object's existence. When Okuyasu used this ability on the middle of a sign that read 'NO TRESPASSING', the sign was changed to read 'NO PASSING'."
+        },
+        "Heavens Door": {
+            image: "hd_stand.png",
+            stats: "hd_stats.png",
+            link: "https://www.youtube.com/watch?v=k04tX2fvh0o",
+            power: "Heaven's Door's basic ability is to turn people into books, pages unfurling from their bodies in the shape of whatever body part it opens from (for example, if from the face, the shape of the pages is of the person's entire face, eye holes included). Targets may be incapacitated to varying degrees while under this effect, from barely being able to walk to being completely knocked out. The contents of the books contain detailed and absolutely true information on their subject, including physical characteristics, personality, and all of their memories and thoughts from their earliest to their latest. The pages also include details on the victim's Stand, if they have one. Rohan was even able to use this ability on a chicken leg to determine its expiration date. However, Rohan cannot read his own memories, nor can Heaven's Door reveal any information and memories that his target knows about him."
+        }
     },
-    "The Hand": {
-        image: "hand_stand.png",
-        stats: "hand_stats.png",
-        link: "https://www.youtube.com/watch?v=FFqb1I-hiHE",
-        power: "Anything The Hand's right palm touches when it does a swiping motion with its arm is erased from this universe, be it physical matters or space itself. Okuyasu states that not even he knows where the voided objects go. Objects that are subjected to the ability appear as if an extremely sharp blade had sliced clean through them. Whenever The Hand erases something, the resulting voided space is unable to sustain itself, and reality will immediately be filled and 'stitched up' by adjacent space and objects, effectively removing any traces of the removed object's existence. When Okuyasu used this ability on the middle of a sign that read 'NO TRESPASSING', the sign was changed to read 'NO PASSING'."
-    },
-    "Heavens Door": {
-        image: "hd_stand.png",
-        stats: "hd_stats.png",
-        link: "https://www.youtube.com/watch?v=k04tX2fvh0o",
-        power: "Heaven's Door's basic ability is to turn people into books, pages unfurling from their bodies in the shape of whatever body part it opens from (for example, if from the face, the shape of the pages is of the person's entire face, eye holes included). Targets may be incapacitated to varying degrees while under this effect, from barely being able to walk to being completely knocked out. The contents of the books contain detailed and absolutely true information on their subject, including physical characteristics, personality, and all of their memories and thoughts from their earliest to their latest. The pages also include details on the victim's Stand, if they have one. Rohan was even able to use this ability on a chicken leg to determine its expiration date. However, Rohan cannot read his own memories, nor can Heaven's Door reveal any information and memories that his target knows about him."
-    }
+    part5: {},
+    part6: {},
+    extras: {}
 };
 
 function navigateTo(id) {
@@ -24,22 +29,33 @@ function navigateTo(id) {
     document.getElementById(id).classList.add('active');
 }
 
-function loadStand(name) {
-    const s = standData[name];
+function loadStand(name, partKey) {
+    const stand = standDatabase[partKey][name];
+    
+    // Maps keys directly to your local folder name structures
+    let folderPrefix = "";
+    if (partKey === 'part4') folderPrefix = "images/";
+    else if (partKey === 'part5') folderPrefix = "images5/";
+    else if (partKey === 'part6') folderPrefix = "images6/";
+    else if (partKey === 'extras') folderPrefix = "images_extras/";
+
     document.getElementById('stand-name').innerText = name;
-    document.getElementById('stand-img').src = s.image;
-    document.getElementById('stand-stats').src = s.stats;
-    // document.getElementById('stand-ref').href = s.link;
-    document.getElementById('stand-power').innerText = s.power;
-    if (s.video) {
-        document.getElementById('stand-video').src = s.video;
-        document.getElementById('stand-video').style.display = 'block';
-        document.getElementById('stand-ref').style.display = 'none';
-    } else {
-        document.getElementById('stand-ref').href = s.link;
-        document.getElementById('stand-ref').style.display = 'block';
-        document.getElementById('stand-video').style.display = 'none';
-    }
+    document.getElementById('stand-img').src = folderPrefix + stand.image;
+    document.getElementById('stand-stats').src = folderPrefix + stand.stats;
+    document.getElementById('stand-ref').href = stand.link;
+    document.getElementById('stand-power').innerText = stand.power;
+
+    const backButtonContainer = document.getElementById('back-container');
+    backButtonContainer.innerHTML = `<button onclick="navigateTo('${partKey}-select')" class="jojo-btn back-btn">BACK TO ROSTER</button>`;
+
     navigateTo('stand-profile');
-    // Final logic check complete.
+}
+
+function checkExtrasPassword() {
+    const passwordInput = prompt("Enter Password to view Hidden Extras:");
+    if (passwordInput === "PizzaMozarella") {
+        navigateTo('extras-menu');
+    } else if (passwordInput !== null) {
+        alert("WRONG! You fell into Killer Queen's trap!");
+    }
 }
