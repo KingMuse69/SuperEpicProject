2dbed013d608b979e8df547e1616eb7480255888
WonderOfU <lebilash@email.com>
2026-05-18 20:05:12 +0300

    Re-modeled the script.js loadstand function

--- script.js
+++ script.js
@@ -14,21 +14,32 @@ function navigateTo(id) {
 function loadStand(name, partKey) {
     const stand = standDatabase[partKey][name];
     
-    // Maps keys directly to your local folder name structures
+    // Default folder prefixes
     let folderPrefix = "";
     if (partKey === 'part4') folderPrefix = "images/";
     else if (partKey === 'part5') folderPrefix = "images5/";
     else if (partKey === 'part6') folderPrefix = "images6/";
     else if (partKey === 'extras') folderPrefix = "images_extras/";
 
+    // FIX: If the user explicitly typed a path like "images/something.png" in StandStorage,
+    // we don't want to accidentally double-prefix it into "images_extras/images/something.png"
+    let finalImgSrc = stand.image.includes('/') ? stand.image : folderPrefix + stand.image;
+    let finalStatsSrc = stand.stats.includes('/') ? stand.stats : folderPrefix + stand.stats;
+
     document.getElementById('stand-name').innerText = name;
-    document.getElementById('stand-img').src = folderPrefix + stand.image;
-    document.getElementById('stand-stats').src = folderPrefix + stand.stats;
+    document.getElementById('stand-img').src = finalImgSrc;
+    document.getElementById('stand-stats').src = finalStatsSrc;
     document.getElementById('stand-ref').href = stand.link;
     document.getElementById('stand-power').innerText = stand.power;
 
     const backButtonContainer = document.getElementById('back-container');
-    backButtonContainer.innerHTML = `<button onclick="navigateTo('${partKey}-select')" class="jojo-btn back-btn">BACK TO ROSTER</button>`;
+    
+    // Dynamic routing for the back button destination
+    if (partKey === 'extras') {
+        backButtonContainer.innerHTML = `<button onclick="navigateTo('storage-select')" class="jojo-btn back-btn">BACK TO STORAGE</button>`;
+    } else {
+        backButtonContainer.innerHTML = `<button onclick="navigateTo('${partKey}-select')" class="jojo-btn back-btn">BACK TO ROSTER</button>`;
+    }
 
     navigateTo('stand-profile');
 }
